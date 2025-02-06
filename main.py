d0c99f3837c1d2c136458114cbd2c7e76d34de50
Stephen O'Baker <stephengbaker@yahoo.com>
2025-02-06 15:49:54 -0600

    findChessBoard corners working on a 7x7

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ img = cv2.imread('captured_image.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Find the chess board corners
-ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+ret, corners = cv2.findChessboardCorners(gray, (7, 7), None)
 
 # if chessboard corners are detected
 if ret == True:
     # Draw and display the corners
-    img = cv2.drawChessboardCorners(img, (8, 8), corners, ret)
+    img = cv2.drawChessboardCorners(img, (7, 7), corners, ret)
     cv2.imshow('Chessboard', img)
     cv2.waitKey(0)
 cv2.destroyAllWindows()
